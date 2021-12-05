34e36cc6ae766ba466b5f4bd577cdb922317ba1d
smyzin <smyzin.work@gmail.com>
2021-12-05 22:03:22 +0300

    Initial Commit

---
+++ AppleMusic/AppleMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicApp.swift
+//  AppleMusic
+//
+//  Created by Sergey Myzin on 05.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
